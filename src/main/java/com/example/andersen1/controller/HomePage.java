2dd3e6d13a35d74ddf25d59678ade0063972dc18
Вячеслав Ким <volosgreeck@gmail.com>
2021-12-09 16:08:58 +0300

    changing Controller to RestController

--- src/main/java/com/example/andersen1/controller/HomePage.java
+++ src/main/java/com/example/andersen1/controller/HomePage.java
@@ -3,8 +3,9 @@ package com.example.andersen1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/index")
 public class HomePage {
 
