ad2221d3632272736049aae8778ab9d308afc5c5
Вячеслав Ким <volosgreeck@gmail.com>
2021-12-09 15:17:23 +0300

    adding tomcat dependency and war packaging

--- src/main/java/com/example/andersen1/Andersen1Application.java
+++ src/main/java/com/example/andersen1/Andersen1Application.java
@@ -2,9 +2,10 @@ package com.example.andersen1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Andersen1Application {
+public class Andersen1Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Andersen1Application.class, args);
     }
